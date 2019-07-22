c3b5453a869e9a5d9c5ac798a865446c9af9674c
Jennifer Lipps <jjlipps@gmail.com>
2019-07-21 21:09:50 -0500

    wip, navbar options displaying

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+
+import Home from '../components/Home/Home';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-info'>test</button>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MyNavbar />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NewGame/NewGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NewGame.scss';
+
+class NewGame extends React.Component {
+  render() {
+    return (
+      <div>placeholder</div>
+    );
+  }
+}
+
+export default NewGame;
