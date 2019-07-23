830a4bef34ca5e7b5136056bad8e28b261db4037
Jennifer Lipps <jjlipps@gmail.com>
2019-07-22 20:58:50 -0500

    seed data complete

--- src/App/App.js
+++ src/App/App.js
@@ -65,6 +65,7 @@ class App extends React.Component {
                 <Switch>
                   <PublicRoute path='/auth' component={Auth} authed={authed} />
                   <PrivateRoute path='/home' component={Home} authed={authed} />
+                  <PrivateRoute path='/gameslibrary' component={GamesLibrary} authed={authed} />
                   <Redirect from="*" to="/auth" />
                 </Switch>
               </div>
