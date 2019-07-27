7e6f9fab8950e9fc3f7e531df209cd1fc934beb4
Jennifer Lipps <jjlipps@gmail.com>
2019-07-27 10:26:38 -0500

    wip

--- src/components/NewGame/NewGame.js
+++ src/components/NewGame/NewGame.js
@@ -1,12 +1,77 @@
 import React from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
 
 import './NewGame.scss';
+
+const defaultGame = {
+  title: '',
+  typeId: '',
+  maker: '',
+  minPlayers: '',
+  maxPlayers: '',
+};
 
 class NewGame extends React.Component {
   render() {
     return (
       <div className="NewGame">
-        <h1>New Game</h1>
+        <h1>Create a New Game</h1>
+        <Form>
+        <FormGroup>
+          <Label for="title">Game Title</Label>
+          <Input
+          type="text"
+          id="title"
+          placeholder="New Title"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="maker">Maker</Label>
+          <Input
+          type="text"
+          id="maker"
+          placeholder="Produced the game"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="minPlayers">Minimum Players</Label>
+          <Input
+          type="text"
+          id="minPlayers"
+          placeholder="2"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="maxPlayers">Maximum Players</Label>
+          <Input
+          type="text"
+          id="maxPlayers"
+          placeholder="6"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="typeId">Select Type of Game</Label>
+          <Input type="select" name="select" id="typeId">
+            <option>type1</option>
+            <option>type2</option>
+            <option>type3</option>
+            <option>type4</option>
+            <option>type5</option>
+            <option>type6</option>
+            <option>type7</option>
+            <option>type8</option>
+            <option>type9</option>
+            <option>type10</option>
+          </Input>
+        </FormGroup>
+        <Button>Add This Game</Button>
+      </Form>
       </div>
     );
   }
